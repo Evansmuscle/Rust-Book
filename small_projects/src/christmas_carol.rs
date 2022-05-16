ee5683d9a8420a44fdcda08e8c3a42262c1058cb
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-05-16 17:59:49 +0300

    feat: Finish christmas carol printer

--- small_projects/src/christmas_carol.rs
+++ small_projects/src/christmas_carol.rs
@@ -1,12 +1,44 @@
+fn numeriser(i: usize) -> String {
+    if i == 1 {
+        return String::from("first");
+    }
+
+    if i == 2 {
+        return String::from("second");
+    }
+
+    if i == 3 {
+        return String::from("third");
+    }
+
+    let numerised_value = format!("{}th", i);
+    return numerised_value;
+}
+
 pub fn christmas_carol() {
     let repeated_lines = [
         "Two candy canes", "Three boughs of holly", "Four colored lights", "A shining star", "Little silver bells", "Candles a-glowing", "Gold and silver tinsel", "A guardian angel", "Some mistletoe", "Gifts for one and all", "All their good wishes"
     ];
     
-    for idx in 0..=repeated_lines.len() {
-        println!("On the {}th day of Christmas", idx + 1);
-        for rev_idx in (0..=idx).rev() {
-            
+    for idx in 0..repeated_lines.len() {
+        println!("On the {} day of Christmas", numeriser(idx + 1));
+        println!("My good friends brought to me");
+        if idx != 0 {
+            for rev_idx in (0..=idx).rev() {
+                let rev_idx_cut: isize = rev_idx as isize - 1;
+                
+                if rev_idx_cut < 0 {
+                    continue;
+                }
+                
+                if rev_idx_cut == 0 {
+                    println!("{}", repeated_lines[0]);
+                    continue;
+                }
+                
+                println!("{}", repeated_lines[rev_idx_cut as usize]);
+            }
         }
+        println!("And a song for the christmas tree\n");
     }
 }
